5b66432b3d650a8d55569fbcfe73e615126a3418
Return to interaction <mohammadreyad2011@gmail.com>
2026-04-19 14:50:33 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+// تجربة سيرفر بسيط
+app.get("/", (req, res) => {
+  res.send("Server is working 🔥");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000 🔥");
+});
